828001eb13f923323e7680bdacb68fd5e51e4b92
Chohye Han <choiscafe@gmail.com>
2022-11-16 19:19:52 -0500

    created DetailPage.js

--- client/src/ProductCard.js
+++ client/src/ProductCard.js
@@ -1,38 +1,24 @@
-import { useState } from "react";
-import IngredientsContainer from './IngredientsContainer'
-import ReviewsContainer from './ReviewsContainer'
-import NewReviewForm from './NewReviewForm'
-import BarChart from './components/BarChart'
+import DetailPage from './DetailPage'
 
 function ProductCard({ product, currentUser, addReview }) {
 
-  const [showForm, setShowForm] = useState(false);
+
 
   const {id, name, category, brand, image, price_size, ingredients, reviews } = product
 
-  function handleClick() {
-    setShowForm((showForm) => !showForm);
-  }
  
   return (
     <div className="product-card">
-      <p>Avarage Review: {product.averageScore}</p>
+      
       <img src={image} alt={name} />
       <p>{brand}</p>
       <h4>{name}</h4>
       <p>{category}</p>
+      <p>Avarage Review: {product.averageScore}</p>
       <p>{price_size}</p>
       <h2>Ingredient: </h2>
-      <>
-        <BarChart product={product}/>
-        <IngredientsContainer ingredients={ingredients} />
-      </>
-      <h2>Rating & Reviews</h2>
-        {showForm ? <NewReviewForm addReview={addReview}/> : null}
-        {!showForm ? <div className="buttonContainer">
-          <button onClick={handleClick}>Write a Review</button>
-        </div> : null}
-        <ReviewsContainer reviews={reviews} currentUser={currentUser}/>
+      <DetailPage currentUser={currentUser} addReview={addReview} product={product}/>
+      
     </div>
   )
 }
