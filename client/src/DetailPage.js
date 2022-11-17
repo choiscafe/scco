ef7820f344732ff247810a7dfc929daf854f3c3f
Chohye Han <choiscafe@gmail.com>
2022-11-16 19:19:30 -0500

    created DetailPage.js

---
+++ client/src/DetailPage.js
@@ -0,0 +1,39 @@
+import { useState } from "react";
+import IngredientsContainer from './IngredientsContainer'
+import ReviewsContainer from './ReviewsContainer'
+import NewReviewForm from './NewReviewForm'
+import BarChart from './components/BarChart'
+
+function DetailPage({product, currentUser, addReview}){
+
+  const {id, name, category, brand, image, price_size, ingredients, reviews } = product
+  const [showForm, setShowForm] = useState(false);
+
+
+  function handleClick() {
+    setShowForm((showForm) => !showForm);
+  }
+
+  return (
+    <div className="product-card">
+      <img src={image} alt={name} />
+      <p>{brand}</p>
+      <h4>{name}</h4>
+      <p>{category}</p>
+      <p>Avarage Review: {product.averageScore}</p>
+      <p>{price_size}</p>
+      <h2>Ingredient: </h2>
+      <>
+        <BarChart product={product}/>
+        <IngredientsContainer ingredients={ingredients} />
+      </>
+      <h2>Rating & Reviews</h2>
+        {showForm ? <NewReviewForm addReview={addReview}/> : null}
+        {!showForm ? <div className="buttonContainer">
+          <button onClick={handleClick}>Write a Review</button>
+        </div> : null}
+        <ReviewsContainer reviews={reviews} currentUser={currentUser}/>
+    </div>
+  )
+}
+export default DetailPage
