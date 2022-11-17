63fddadc69897c3f5e1305dc74f4c14eaf69ba37
Chohye Han <choiscafe@gmail.com>
2022-11-17 12:35:26 -0500

    single ewg inserted

--- client/src/IngredientCard.js
+++ client/src/IngredientCard.js
@@ -1,11 +1,12 @@
+import EWG from './EWG'
 function IngredientCard({ ingredient }){
   
   const {id, name, ewg_rating, toxic, irritant, skin_type, functional} = ingredient
-  
+ 
   return (
     <div className="ingredient-card">
-    <p>{name}</p>
-      <p>{ewg_rating}</p> 
+      <p>{name}</p>
+      <p><EWG ewg_rating={ewg_rating}/></p> 
       <p>{toxic}</p>
       <p>{irritant}</p>
       <p>{skin_type}</p>
